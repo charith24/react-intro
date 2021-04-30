d5da7fcd2a1b58aa2c4acd1a9a4336440987d5b2
charith24 <67182579+charith24@users.noreply.github.com>
2021-04-30 23:40:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> Welcome to my Website </h1>
+    <ol>
+      <li>First one </li>
+      <li>JavaScript</li>
+      <li>CSS </li>
+      <li>HTML </li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
